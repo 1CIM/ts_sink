7182d4a6ecc0662c9381eaf22a5e6de1971d6111
kim_haaga <kim.haaga@live.se>
2021-07-04 23:59:31 +0200

    Update Child so TS know its a react component comments in code

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,6 +2,19 @@ interface ChildProps {
   color: string;
 }
 
-export const Child = ({color}: ChildProps) => {
+// This one  works but second one is proffered
+
+// export const Child = ({color}: ChildProps) => {
+//   return <div data-cy='color' >{color}</div>
+// }
+
+// ================================================
+
+// FC = Function Component
+// Here we tell TS its a React component,
+// So we can assign properties to it like 'propTypes' and 'contextTypes'
+// And 'Child' will receive props of type 'ChildProps' as before 
+
+export const Child: React.FC<ChildProps> = ({color}) => {
   return <div data-cy='color' >{color}</div>
 }
