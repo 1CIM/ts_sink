35d0bec014d64a5023bbd94ee9fa0160d9f758ff
kim_haaga <kim.haaga@live.se>
2021-07-04 23:35:14 +0200

    Add test to see if it works

---
+++ cypress/integration/initialRender.spec.js
@@ -0,0 +1,13 @@
+const { within } = require("@testing-library/react")
+
+describe('User can see initial render', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('shows page header', () => {
+    cy.get('[data-cy=header]').within(() => {
+      cy.get('[data-cy=header-text]').should('contain', 'Hi there')
+    })
+  })
+})
